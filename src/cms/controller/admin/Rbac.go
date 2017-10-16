dc1211b4d2e96f604081a594b331ea11f3a646c7
ming123jew <ming123jew123@qq.com>
2017-10-16 09:27:38 +0800

    a

--- src/cms/controller/admin/Rbac.go
+++ src/cms/controller/admin/Rbac.go
@@ -2,13 +2,25 @@ package admin
 
 import "fmt"
 
-type RbacI interface {
-	CheckPermission()
-	SetPermission()
+
+type RbacPermission struct {
+	Permission  map[string][]string // article : article/get  article/post  article?m=add
+}
+type UserPermission struct {
+	Username 	string
+	Roleid		int
+	Permission  map[string][]string // article : article/get  article/post  article?m=add
 }
 
 type Rbac struct {
 
+}
+type RbacI interface {
+	CheckPermission()
+	SetPermission()
+}
+func NewRbac(rbac *Rbac) {
+	rbac.test()
 }
 
 func (own *Rbac)test() {
